f9887bab4d189cc3fb1c15e1292da0b3ec3623cc
Vika Suhorukova <viktorya.sukhorukova@gmail.com>
2018-01-14 02:07:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function destructivelyAppendKitten(newName) {
+  kittens.push(newName);
+  return kittens;
+}
+
+function destructivelyPrependKitten(newName) {
+  kittens.unshift(newName);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(nameToRemove) {
+  kittens.pop(nameToRemove);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(nameToRemove) {
+  kittens.shift(nameToRemove);
+  return kittens;
+}
+
+function appendKitten(newName) {
+  var newKittensArray = kittens.splice(kittens.length-1,0,newName);
+  return newKittensArray;
+}
