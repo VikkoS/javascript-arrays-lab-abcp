95370ba3bcb104d1c6862eb8c31b939b35469a94
Vika Suhorukova <viktorya.sukhorukova@gmail.com>
2018-01-14 02:16:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ function prependKitten(newName) {
 }
 
 function  removeLastKitten() {
-  var newKittensArray = kittens.slice(0, kittens.length-2);
+  var newKittensArray = kittens.slice(0, kittens.length-1);
   return newKittensArray;
 }
